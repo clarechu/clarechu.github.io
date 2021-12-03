ad3b77b799796163494b3abee183c587f0734c4e
Lei Chu <1062186165@qq.com>
2021-12-03 18:02:54 +0800

    tcc 支持

--- transaction/xa/account-xa/src/main/java/com/example/accountxa/AccountXaApplication.java
+++ transaction/xa/account-xa/src/main/java/com/example/accountxa/AccountXaApplication.java
@@ -4,11 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableDiscoveryClient
+//@EnableEurekaClient
+@EnableFeignClients
 @MapperScan({"com.example.accountxa.dao"})
 public class AccountXaApplication {
 
--- transaction/xa/order-xa/src/test/java/com/example/orderxa/OrderXaApplicationTests.java
+++ transaction/xa/order-xa/src/test/java/com/example/orderxa/OrderXaApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.orderxa;
 
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderXaApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
